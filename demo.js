ef8422a870fa25aebdc83aae2a9b5466771d4d89
Felix Waterstraat | Spreewunder <felix.waterstraat@spreewunder.com>
2024-05-17 09:37:24 +0200

    feat: initial version

---
+++ demo.js
@@ -0,0 +1,8 @@
+export function add(number1, number2){
+    // Please add an implementation
+}
+
+export function subtract(number1, number2){
+    // Please change this implementation causing some conflicts!
+    return number1-number2;
+}
